20232958bfd014d41c34246a0357baaa3cf272c2
mxxxnick <mxwei@bupt.edu.cn>
2020-10-05 19:26:34 +0800

    m

--- leetcode/Demo.java
+++
@@ -1,8 +0,0 @@
-
-public class Demo {
-    public static void main(String[] args) {
-        String s = "LeEeetcode";
-        System.out.println(s.substring(0,1));
-        System.out.println(s.substring(0,1).concat(s.substring(2, s.length())));
-    }    
-}
